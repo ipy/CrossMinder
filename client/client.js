c6bdf74e6ce21fa1fcba7428a2e92123fcc57ea1
Sin <dev@zouxin.net>
2015-10-22 08:02:46 +0800

    add accounts

--- client/client.js
+++ client/client.js
@@ -2,73 +2,79 @@ otType = OTJson0.type;
 otDiff = OTJson0Diff.diff;
 
 Meteor.startup(function() {
-  bootCrossMinder = function(minder, editor, jsonDiff) {
-    var json, initOp, isSubmitting, isPatching;
-
-    doc = null;
-    docId = util.getHashParam('docId');
-    json = minder.exportJson();
-    if(!docId) {
-      docId = json.root.data.id;
-      util.setHashParam('docId', docId);
-    }
-
-    Meteor.call('createDocIfNotExists', docId, [{p: [], oi: json}], function(error, result) {
-      if(error) {
-        alert('Create document failed');
-        throw error
-      };
-
-      doc = result;
-      if(docId !== json.root.data.id) {
-        json = doc.snapshot;
-        minder.importJson(json);
-      }
-
-      minder.on("contentchange", changed);
-      Meteor.subscribe('ops', function() {
-        Tracker.autorun(autorun);
-      });
-    });
-
-    submitOp = function(op) {
-      isSubmitting = true;
-      Meteor.call('submitOp', docId, doc.snapshotVersion, op, function(error, result){
-        isSubmitting = false;
-      });
-    };
-
-    autorun = function() {
-      console.log('start autorun')
-      isPatching = true;
-      var ops = Ops
-        .find({docId: docId, version: {$gt: doc.snapshotVersion}})
-        .fetch()
-        .map(function(op) {
-          return op.op;
-        });
-      if(!ops.length) {
-        isPatching = false;
-        return;
-      }
-      doc.snapshotVersion += ops.length;
-      var op = ops.reduce(function(op1, op2) {
-        return otType.compose(op1, op2);
-      }, []);
-      doc.snapshot = otType.apply(doc.snapshot, op);
-
-      var json = minder.exportJson();
-      var diff = jsonDiff(json, doc.snapshot);
-      minder.applyPatches(diff);
-      setTimeout(function() { isPatching = false; }, 0);
-    };
-
-    changed = function() {
-      console.log('changed')
-      if(isPatching || isSubmitting) return;
-      var json = minder.exportJson();
-      var op = otDiff(doc.snapshot, json);
-      submitOp(op);
-    }
-  };
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+  });
 });
+
+Meteor.subscribe("userData");
+
+bootCrossMinder = function(minder, editor, jsonDiff) {
+  var json, initOp, isSubmitting, isPatching;
+
+  doc = null;
+  docId = util.getHashParam('docId');
+  json = minder.exportJson();
+  if(!docId) {
+    docId = json.root.data.id;
+    util.setHashParam('docId', docId);
+  }
+
+  Meteor.call('createDocIfNotExists', docId, [{p: [], oi: json}], function(error, result) {
+    if(error) {
+      alert('Create document failed');
+      throw error
+    };
+
+    doc = result;
+    if(docId !== json.root.data.id) {
+      json = doc.snapshot;
+      minder.importJson(json);
+    }
+
+    minder.on("contentchange", changed);
+    Meteor.subscribe('ops', function() {
+      Tracker.autorun(autorun);
+    });
+  });
+
+  submitOp = function(op) {
+    isSubmitting = true;
+    Meteor.call('submitOp', docId, doc.snapshotVersion, op, function(error, result){
+      isSubmitting = false;
+    });
+  };
+
+  autorun = function() {
+    console.log('start autorun')
+    isPatching = true;
+    var ops = Ops
+      .find({docId: docId, version: {$gt: doc.snapshotVersion}})
+      .fetch()
+      .map(function(op) {
+        return op.op;
+      });
+    if(!ops.length) {
+      isPatching = false;
+      return;
+    }
+    doc.snapshotVersion += ops.length;
+    var op = ops.reduce(function(op1, op2) {
+      return otType.compose(op1, op2);
+    }, []);
+    doc.snapshot = otType.apply(doc.snapshot, op);
+
+    var json = minder.exportJson();
+    var diff = jsonDiff(json, doc.snapshot);
+    minder.applyPatches(diff);
+    setTimeout(function() { isPatching = false; }, 0);
+  };
+
+  changed = function() {
+    console.log('changed')
+    if(isPatching || isSubmitting) return;
+    var json = minder.exportJson();
+    var op = otDiff(doc.snapshot, json);
+    submitOp(op);
+  }
+};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,14 @@ Meteor.publish("docs", function () {
   return Docs.find();
 });
 
+Meteor.publish("userData", function () {
+  if (this.userId) {
+    return Meteor.users.find({_id: this.userId}, {fields: {'other': 1, 'things': 1}});
+  } else {
+    this.ready();
+  }
+});
+
 Meteor.methods({
   createDocIfNotExists: function(docId, initOp) {
     initOp = initOp || [];
